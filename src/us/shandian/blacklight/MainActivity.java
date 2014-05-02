beca599c642802418a334ae94490d5ab9f1aa151
PeterCxy <xqsx43cxy@126.com>
2014-05-02 23:56:03 +0800

    Initial commit

---
+++ src/us/shandian/blacklight/MainActivity.java
@@ -0,0 +1,17 @@
+package us.shandian.blacklight;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
