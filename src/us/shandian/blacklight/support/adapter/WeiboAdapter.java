79b100b060f70d80b83ace346e62854cd2e426aa
PeterCxy <xqsx43cxy@126.com>
2014-05-08 13:15:12 +0800

    WeiboAdapter: Download Avatars

--- src/us/shandian/blacklight/support/adapter/WeiboAdapter.java
+++ src/us/shandian/blacklight/support/adapter/WeiboAdapter.java
@@ -1,14 +1,18 @@
 package us.shandian.blacklight.support.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.os.AsyncTask;
 import android.text.Html;
 
 import us.shandian.blacklight.R;
+import us.shandian.blacklight.cache.user.UserApiCache;
 import us.shandian.blacklight.model.MessageModel;
 import us.shandian.blacklight.model.MessageListModel;
 import us.shandian.blacklight.support.StatusTimeUtils;
@@ -18,11 +22,13 @@ public class WeiboAdapter extends BaseAdapter
 	private MessageListModel mList;
 	private LayoutInflater mInflater;
 	private StatusTimeUtils mTimeUtils;
+	private UserApiCache mUserApi;
 	
 	public WeiboAdapter(Context context, MessageListModel list) {
 		mList = list;
 		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		mTimeUtils = StatusTimeUtils.instance(context);
+		mUserApi = new UserApiCache(context);
 	}
 	
 	@Override
@@ -46,24 +52,67 @@ public class WeiboAdapter extends BaseAdapter
 			return convertView;
 		} else {
 			MessageModel msg = mList.get(position);
-			View v = mInflater.inflate(R.layout.weibo, null);
-			v.setTag(msg);
-			TextView name = (TextView) v.findViewById(R.id.weibo_name);
-			TextView date = (TextView) v.findViewById(R.id.weibo_date);
-			TextView from = (TextView) v.findViewById(R.id.weibo_from);
-			TextView retweet = (TextView) v.findViewById(R.id.weibo_retweet);
-			TextView comments = (TextView) v.findViewById(R.id.weibo_comments);
-			TextView content = (TextView) v.findViewById(R.id.weibo_content);
-			name.setText(msg.user.screen_name);
-			date.setText(mTimeUtils.buildTimeString(msg.created_at));
-			from.setText(Html.fromHtml(msg.source).toString());
-			retweet.setText(String.valueOf(msg.reposts_count));
-			comments.setText(String.valueOf(msg.comments_count));
-			content.setText(msg.text); // TODO Spannable String , Emoticons
-			
-			// TODO Async Task to download pictures and avatars
-			return v;
+			return bindView(msg);
 		}
+	}
+	
+	private View bindView(MessageModel msg) {
+		View v = mInflater.inflate(R.layout.weibo, null);
+		TextView name = (TextView) v.findViewById(R.id.weibo_name);
+		TextView date = (TextView) v.findViewById(R.id.weibo_date);
+		TextView from = (TextView) v.findViewById(R.id.weibo_from);
+		TextView retweet = (TextView) v.findViewById(R.id.weibo_retweet);
+		TextView comments = (TextView) v.findViewById(R.id.weibo_comments);
+		TextView content = (TextView) v.findViewById(R.id.weibo_content);
+		name.setText(msg.user.screen_name);
+		date.setText(mTimeUtils.buildTimeString(msg.created_at));
+		from.setText(Html.fromHtml(msg.source).toString());
+		retweet.setText(String.valueOf(msg.reposts_count));
+		comments.setText(String.valueOf(msg.comments_count));
+		content.setText(msg.text); // TODO Spannable String , Emoticons
+
+		new ImageDownloader().execute(new Object[]{v, msg});
+		
+		return v;
+	}
+	
+	// Downloads images including avatars
+	private class ImageDownloader extends AsyncTask<Object, Object, Void> {
+
+		@Override
+		protected Void doInBackground(Object[] params) {
+			View v = (View) params[0];
+			MessageModel msg = (MessageModel) params[1];
+			
+			// Avatars
+			if (v != null) {
+				Bitmap avatar = mUserApi.getSmallAvatar(msg.user);
+				publishProgress(new Object[]{0, avatar, v});
+			}
+			
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(Object[] values) {
+			super.onProgressUpdate(values);
+			
+			switch ((int) values[0]) {
+				case 0:
+					Bitmap avatar = (Bitmap) values[1];
+					View v = (View) values[2];
+					if (v != null) {
+						ImageView iv = (ImageView) v.findViewById(R.id.weibo_avatar);
+						if (iv != null) {
+							iv.setImageBitmap(avatar);
+						}
+					}
+					
+			}
+			
+		}
+
+		
 	}
 
 }
