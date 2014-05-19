3ae0f0b3438affbd5ed61d43ce2809d5125fb64f
PeterCxy <xqsx43cxy@126.com>
2014-05-19 21:07:48 +0800

    HomeTimeLineApiCache: Increase max decode size

--- src/us/shandian/blacklight/cache/statuses/HomeTimeLineApiCache.java
+++ src/us/shandian/blacklight/cache/statuses/HomeTimeLineApiCache.java
@@ -154,7 +154,7 @@ public class HomeTimeLineApiCache
 			BitmapFactory.Options opt = new BitmapFactory.Options();
 			opt.inJustDecodeBounds = true;
 			BitmapFactory.decodeByteArray(cache, 0, cache.length, opt);
-			opt.inSampleSize = Utility.computeSampleSize(opt, -1, 1024 * 1024);
+			opt.inSampleSize = Utility.computeSampleSize(opt, -1, 4 * 1024 * 1024);
 			opt.inJustDecodeBounds = false;
 			return BitmapFactory.decodeByteArray(cache, 0, cache.length, opt);
 		}
