27b99f96e989dda9dbe41ac77e7b88d0694dc02f
PeterCxy <xqsx43cxy@126.com>
2014-05-31 17:11:19 +0800

    WeiboAdapter: Clean up

--- src/us/shandian/blacklight/support/adapter/WeiboAdapter.java
+++ src/us/shandian/blacklight/support/adapter/WeiboAdapter.java
@@ -29,7 +29,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
