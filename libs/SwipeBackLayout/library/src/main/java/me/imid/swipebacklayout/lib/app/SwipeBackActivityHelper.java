2d898e98290f2e6ff73b46b19f015ed9401b677f
Peter Cai <xqsx43cxy@126.com>
2014-08-09 10:58:16 +0800

    SwipeBackLayout: import with non-submodule form

---
+++ libs/SwipeBackLayout/library/src/main/java/me/imid/swipebacklayout/lib/app/SwipeBackActivityHelper.java
@@ -0,0 +1,66 @@
+
+package me.imid.swipebacklayout.lib.app;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.Utils;
+
+/**
+ * @author Yrom
+ */
+public class SwipeBackActivityHelper {
+    private Activity mActivity;
+
+    private SwipeBackLayout mSwipeBackLayout;
+
+    public SwipeBackActivityHelper(Activity activity) {
+        mActivity = activity;
+    }
+
+    @SuppressWarnings("deprecation")
+    public void onActivityCreate() {
+        mActivity.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mActivity.getWindow().getDecorView().setBackgroundDrawable(null);
+        mSwipeBackLayout = (SwipeBackLayout) LayoutInflater.from(mActivity).inflate(
+                me.imid.swipebacklayout.lib.R.layout.swipeback_layout, null);
+        mSwipeBackLayout.addSwipeListener(new SwipeBackLayout.SwipeListener() {
+            @Override
+            public void onScrollStateChange(int state, float scrollPercent) {
+                if (state == SwipeBackLayout.STATE_IDLE && scrollPercent == 0) {
+                    Utils.convertActivityFromTranslucent(mActivity);
+                }
+            }
+
+            @Override
+            public void onEdgeTouch(int edgeFlag) {
+                Utils.convertActivityToTranslucent(mActivity);
+            }
+
+            @Override
+            public void onScrollOverThreshold() {
+
+            }
+        });
+    }
+
+    public void onPostCreate() {
+        mSwipeBackLayout.attachToActivity(mActivity);
+        Utils.convertActivityFromTranslucent(mActivity);
+    }
+
+    public View findViewById(int id) {
+        if (mSwipeBackLayout != null) {
+            return mSwipeBackLayout.findViewById(id);
+        }
+        return null;
+    }
+
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mSwipeBackLayout;
+    }
+}
