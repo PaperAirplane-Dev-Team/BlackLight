b6f440022345e03cf187e5e30a4e46a07829eb2d
Xavier Yao <xavieryao@live.cn>
2015-08-06 21:50:57 +0800

    Randomly arrange developer list
    With Peter Cai excluded.
    Signed-off-by: Xavier Yao <xavieryao@live.cn>

--- blacklight-base/src/main/java/info/papdt/blacklight/ui/settings/DevelopersActivity.java
+++ blacklight-base/src/main/java/info/papdt/blacklight/ui/settings/DevelopersActivity.java
@@ -21,6 +21,7 @@ package info.papdt.blacklight.ui.settings;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,11 +29,14 @@ import android.view.ViewGroup;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.Random;
+
 import info.papdt.blacklight.R;
 import info.papdt.blacklight.api.user.UserApi;
 import info.papdt.blacklight.model.UserListModel;
 import info.papdt.blacklight.model.UserModel;
 import info.papdt.blacklight.support.AsyncTask;
+import info.papdt.blacklight.support.LogF;
 import info.papdt.blacklight.support.Utility;
 import info.papdt.blacklight.support.adapter.UserAdapter;
 import info.papdt.blacklight.ui.common.AbsActivity;
@@ -62,19 +66,36 @@ public class DevelopersActivity extends AbsActivity
 
 	private class UserGetter extends AsyncTask<Void, Void, Boolean>{
 
+		private void addDeveloper(String uid){
+			try{
+				UserModel m = UserApi.getUser(uid);
+				if (m != null) {
+					mUserListOfDevelopers.getList().add(m);
+				}
+			} catch(Exception e) {
+
+			}
+		}
+
 		@Override
 		protected Boolean doInBackground(Void... args) {
-			String[] developerWeiboUids=getResources().getStringArray(R.array.developer_weibo_uids);
-			for(String uid : developerWeiboUids){
-				try{
-					UserModel m = UserApi.getUser(uid);
-					if (m != null) {
-						mUserListOfDevelopers.getList().add(m);
-					}
-				} catch(Exception e) {
-
-				}
+			String[] developerWeiboUids = getResources().getStringArray(R.array.developer_weibo_uids);
+			addDeveloper(developerWeiboUids[0]);
+			int developerNum = developerWeiboUids.length - 3;
+			boolean[] added = new boolean[developerNum + 1];
+			int addNum = 0;
+			Random r = new Random();
+			while (addNum != developerNum){
+				int n ;
+				do {
+					n = r.nextInt(developerNum) + 1;
+				} while (added[n]);
+				addDeveloper(developerWeiboUids[n]);
+				added[n] = true;
+				addNum++;
 			}
+			addDeveloper(developerWeiboUids[developerNum + 1]);
+			addDeveloper(developerWeiboUids[developerNum + 2]);
 			return true;
 		}
 
