a4d83d71e85ee523f08f072cab8d5fc2a67980e9
seasonyuu <seasonyuu@gmail.com>
2015-08-11 12:30:00 +0800

    Fix the size of FAB is not same in different android version
    Signed-off-by: Peter Cai <xqsx43cxy@126.com>

--- blacklight-base/src/main/java/info/papdt/blacklight/ui/common/FloatingActionButton.java
+++ blacklight-base/src/main/java/info/papdt/blacklight/ui/common/FloatingActionButton.java
@@ -128,7 +128,10 @@ public class FloatingActionButton extends View implements Animator.AnimatorListe
 	}
 
 	private int getRealWidth() {
-		return getWidth() - getPaddingLeft() - getPaddingRight();
+		if(Build.VERSION.SDK_INT>=21)
+			return getWidth() - getPaddingLeft() - getPaddingRight();
+		else
+			return getWidth();
 	}
 
 	private int getRealHeight() {
