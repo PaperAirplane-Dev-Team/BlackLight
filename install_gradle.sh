8aa418229caae964e0fa821adb9a78b790a87ae0
Harry Chen <373685963@qq.com>
2015-08-18 18:08:15 +0800

    Fix Travis CI again
    Signed-off-by: Harry Chen <373685963@qq.com>

--- install_gradle.sh
+++ install_gradle.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 if [ ! -d "$HOME/gradle-2.2.1" ]; then
+  pushd $HOME;
   wget http://services.gradle.org/distributions/gradle-2.2.1-all.zip;
   unzip gradle-2.2.1-all.zip >> /dev/null;
+  popd;
 else
   echo 'Using cached directory.';
 fi
