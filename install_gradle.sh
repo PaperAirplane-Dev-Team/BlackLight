84b5974f5741bf815161a923aa228e62eb8da2e1
Harry Chen <i@harrychen.xyz>
2016-01-28 11:25:22 +0800

    Cache more files and update gradle version
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- install_gradle.sh
+++ install_gradle.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-if [ ! -d "$HOME/gradle-2.2.1" ]; then
+if [ ! -d "$HOME/gradle-2.10" ]; then
   pushd $HOME;
-  wget http://services.gradle.org/distributions/gradle-2.2.1-all.zip;
-  unzip gradle-2.2.1-all.zip >> /dev/null;
+  wget http://services.gradle.org/distributions/gradle-2.10-all.zip;
+  unzip gradle-2.10-all.zip >> /dev/null;
   popd;
 else
   echo 'Using cached directory.';
 fi
 
-export GRADLE_HOME=$HOME/gradle-2.2.1
+export GRADLE_HOME=$HOME/gradle-2.10
 export PATH=$GRADLE_HOME/bin:$PATH
